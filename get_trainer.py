23bf205341a0bdf5808c7403c26903b4f629d26d
KlaussMarchi <marchiklauss@hotmail.com>
2026-06-01 12:39:17 -0300

    atualizei com marlim

--- get_trainer.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("Model/Analysis.ipynb", "r") as f:
-    nb = json.load(f)
-
-for cell in nb["cells"]:
-    if cell["cell_type"] == "code" and "class Trainer:" in "".join(cell["source"]):
-        print("".join(cell["source"]))
-
